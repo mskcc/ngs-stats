fd4aac143312899395897eb75a85acc7684e20d9
David McManamon <dmcmanam@gmail.com>
2019-04-16 10:51:36 -0400

    fastq.gz table and crawler setup

---
+++ src/test/java/org/mskcc/sequencer/model/ArchivedFastqTest.java
@@ -0,0 +1,28 @@
+package org.mskcc.sequencer.model;
+
+import org.junit.jupiter.api.Test;
+import sun.jvm.hotspot.debugger.arm.ARMThreadContext;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArchivedFastqTest {
+
+    @Test
+    void walkDirectory() throws IOException {
+        List<ArchivedFastq> fastqs = ArchivedFastq.walkDirectory("src/test/resources/fastqArchive/",  "PITT_0999_AH5357BBXY_A2");
+        for (ArchivedFastq f : fastqs) {
+            if (f.getProject() == null) { // Undetermined
+                assertEquals("Undetermined_S0_R1_001.fastq.gz", new File(f.getFastq()).getName());
+            } else {
+                assertEquals("09245_E", f.getProject());
+                assertEquals("S17-33024_IGO_09245_E_21", f.getSample());
+                assertEquals("PITT_0999_AH5357BBXY_A2", f.getRunBaseDirectory());
+                assertEquals("PITT_0999_AH5357BBXY", f.getRun());
+            }
+        }
+    }
+}
