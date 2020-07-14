5f8a44ebf007fa3a65ed7419bc02d29874ab2ccf
David McManamon <dmcmanam@gmail.com>
2020-07-14 09:31:33 -0400

    added, calling crontab twice

---
+++ src/main/resources/interop_parser/settings_igo.py
@@ -0,0 +1,5 @@
+LIMSREST_BASE_URL = 'https://igolims.mskcc.org:8443/LimsRest/'
+HISEQ_ARCHIVE_PATH = "/ifs/archive/GCL/hiseq/"
+HISEQ_INPUT_PATH = "/igo/sequencers/"
+MACHINES = ("brad","diana","jax","johnsawyers","kim","michelle","momo","pitt","scott","toms","vic","ayyan")
+NOVASEQ = ("michelle","diana","a00227")
