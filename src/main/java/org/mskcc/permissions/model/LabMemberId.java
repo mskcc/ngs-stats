94bbdc0d067ad89e7c14d013288d34a36758cc00
David McManamon <dmcmanam@gmail.com>
2021-01-26 10:38:43 -0500

    added NoArgs constructor

--- src/main/java/org/mskcc/permissions/model/LabMemberId.java
+++ src/main/java/org/mskcc/permissions/model/LabMemberId.java
@@ -2,10 +2,12 @@ package org.mskcc.permissions.model;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode
 public class LabMemberId implements Serializable {
     private String pi;
